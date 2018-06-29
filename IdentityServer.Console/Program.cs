7af8d0b7d1cb02bd37cc528c538756084fd0b52f
onwuzulike emeka <onwuzulikee1@gmail.com>
2018-06-29 13:45:17 +0100

    initial commit

---
+++ IdentityServer.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IdentityServerTest.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerTest.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
