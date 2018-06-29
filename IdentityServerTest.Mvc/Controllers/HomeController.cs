6f0e6751a486aa4dcadfef3847d52f00c43b392e
Adebayomi Farinde <Bayo@LAPTOP-6ETB857T>
2018-06-29 16:23:31 +0100

    added some changes

--- IdentityServerTest.Mvc/Controllers/HomeController.cs
+++ IdentityServerTest.Mvc/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 using IdentityServerTest.Mvc.Models;
 
 namespace IdentityServerTest.Mvc.Controllers
@@ -32,6 +32,33 @@ namespace IdentityServerTest.Mvc.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> CheckingForAuthorization()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                string idToken = await HttpContext.GetTokenAsync("id_token");
+
+                var client = new HttpClient();
+                //client.SetBearerToken(accessToken);
+                //    var content = await client.GetStringAsync("http://localhost:6001/api/identity");
+                //    ViewBag.Json = JArray.Parse(content).ToString();
+                //    return View("Contact");
+
+                // Now you can use them. For more info on when and how to use the 
+                // access_token and id_token, see https://auth0.com/docs/tokens
+
+            }
+            return View("Contact");
+
         }
     }
 }
--- IdentityServerTest.Mvc/Startup.cs
+++ IdentityServerTest.Mvc/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace IdentityServerTest.Mvc
 {
@@ -22,6 +25,60 @@ namespace IdentityServerTest.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddOpenIdConnect("oidc", options => {
+
+                    // Set all your OIDC options...
+
+                    options.SignInScheme = "Cookies";
+                    options.Authority = "http://localhost:8080/";
+                    options.RequireHttpsMetadata = false;
+                    options.ClientId = "identity.server.test.mvc";
+                    options.ClientSecret = "secret";
+
+                    // and then set SaveTokens to save tokens to the AuthenticationProperties
+
+                    options.ResponseType = "code id_token";
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.SaveTokens = true;
+
+                    options.Scope.Add("identity.server.test.mvc.api");
+                    options.Scope.Add("offline_access");
+
+
+                    options.SaveTokens = true;
+                });
+
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultScheme = "Cookies";
+            //    options.DefaultChallengeScheme = "oidc";
+            //})
+            //    .AddCookie("Cookies")
+            //    .AddOpenIdConnect("oidc", options =>
+            //    {
+            //        options.SignInScheme = "Cookies";
+            //        options.Authority = "http://localhost:8080/";
+            //        options.RequireHttpsMetadata = false;
+            //        options.ClientId = "identity.server.test.mvc";
+            //        options.ClientSecret = "secret";
+            //        options.ResponseType = "code id_token";
+            //        options.GetClaimsFromUserInfoEndpoint = true;
+            //        options.SaveTokens = true;
+
+            //        options.Scope.Add("identity.server.test.mvc.api");
+            //        options.Scope.Add("offline_access");
+            //    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
