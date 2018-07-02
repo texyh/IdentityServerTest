3433423860f05c6c80eb47881093e2a70c070cb9
Gbolahan Allen <allenforjob@gmail.com>
2018-07-02 12:43:32 +0100

    add console app

--- IdentityServer.ConsoleApp/Program.cs
+++ IdentityServer.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace IdentityServer.ConsoleApp
 {
@@ -6,7 +10,48 @@ namespace IdentityServer.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var task = MainAsync();
+
+            task.Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("https://localhost:44338");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "Identity.Console.App", "tUsRLDV0w/rBgwDmmrFKZDoiL6YRqjyQuLR1NmF8YCo=");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("identityApi");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:54065/api/names");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
